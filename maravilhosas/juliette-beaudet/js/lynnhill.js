707eb1376a34ba7a7dca3e0ca070dc4c213e12da
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2019-12-17 14:20:37 -0300

    Primeira versao do projeto final

--- maravilhosas/juliette-beaudet/js/lynnhill.js
+++ maravilhosas/juliette-beaudet/js/lynnhill.js
@@ -6,3 +6,30 @@ autoraBtn.classList.toggle('arrow-close')
 const autoraInfo = document.querySelector('.secao-autora__about')
 autoraInfo.classList.toggle('closed');
 })
+
+(function() {
+  var menuResponsivo = document.getElementById("menuResponsivo");
+  var btnResponsivo = document.getElementById("btnResponsivo");
+
+  btnResponsivo.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.body.scrollTop += 0;
+    if (menuResponsivo.className === "navegacao__itens") {
+      menuResponsivo.className += " ativo";
+    } else {
+      menuResponsivo.className = "navegacao__itens";
+    }
+  });
+})();
+
+window.addEventListener("scroll", function() {
+  if (pageYOffset >= 250) {
+    document.querySelector(".navegacao").style.backgroundColor = "#CC9933";
+    document.querySelector(".navegacao__itens").style.backgroundColor =
+      "#CC9933";
+  } else {
+    document.querySelector(".navegacao").style.backgroundColor = "transparent";
+    document.querySelector(".navegacao__itens").style.backgroundColor =
+      "transparent";
+  }
+});
