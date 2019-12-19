65b88009a515c0f661274d0203c51bf977915406
Seu nome <gisaamuniz@hotmail.com>
2019-12-19 18:37:03 -0300

    add meu projeto final nao concluido

---
+++ maravilhosas/gisele-souza/js/autoraSeta.js
@@ -0,0 +1,18 @@
+const autoraBtn = document.querySelector('.arrow')
+autoraBtn.addEventListener('click', function(e){
+e.preventDefault()
+autoraBtn.classList.toggle('arrow-close')
+})
+
+const autoraBtn = document.querySelector('.arrow')
+autoraBtn.addEventListener('click', function(e){
+e.preventDefault()
+autoraBtn.classList.toggle('arrow-close')
+const autoraInfo = document.querySelector('.secao-autora__about')
+autoraInfo.classList.toggle('closed');
+})
+  <div class="secao-cta">
+    <a class="btn btn_roxo btn_center" href="../../index.html#mulheres-maravilhosas" target="_self">
+      Ver todas elas
+    </a>
+  </div>
