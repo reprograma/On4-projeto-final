6491494d67135309a7e3139b12c94667d6609de2
Brenda <brenda-pereiraornelas@hotmail.com>
2019-12-21 10:52:30 -0300

    adicionando o projeto final

--- maravilhosas/brenda-pereira/js/autoraSeta.js
+++ maravilhosas/brenda-pereira/js/autoraSeta.js
@@ -1,26 +1,8 @@
-(function() {
-  var menuResponsivo = document.getElementById("menuResponsivo");
-  var btnResponsivo = document.getElementById("btnResponsivo");
 
-  btnResponsivo.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.body.scrollTop += 0;
-    if (menuResponsivo.className === "navegacao__itens") {
-      menuResponsivo.className += " ativo";
-    } else {
-      menuResponsivo.className = "navegacao__itens";
-    }
-  });
-})();
-
-window.addEventListener("scroll", function() {
-  if (pageYOffset >= 250) {
-    document.querySelector(".navegacao").style.backgroundColor = "#674582";
-    document.querySelector(".navegacao__itens").style.backgroundColor =
-      "#674582";
-  } else {
-    document.querySelector(".navegacao").style.backgroundColor = "transparent";
-    document.querySelector(".navegacao__itens").style.backgroundColor =
-      "transparent";
-  }
-});
+const autoraBtn = document.querySelector('.arrow')
+autoraBtn.addEventListener('click', function(e){
+  e.preventDefault()
+  autoraBtn.classList.toggle('arrow-close')
+  const autoraInfo = document.querySelector('.secao-autora__about')
+  autoraInfo.classList.toggle('closed');
+})
