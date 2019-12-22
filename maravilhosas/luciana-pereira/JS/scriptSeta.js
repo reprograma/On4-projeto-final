48e29c48dd27f310cc5b8b6f673751ce4fce1c6f
Lu-Pereira <luci.lv14@gmail.com>
2019-12-21 22:08:49 -0300

    atualização do projeto final, contendo site sobre Ada Lovelace

--- maravilhosas/luciana-pereira/JS/scriptSeta.js
+++ maravilhosas/luciana-pereira/JS/scriptSeta.js
@@ -1,14 +1,14 @@
 const autoraBtn = document.querySelector('.arrow')
 autoraBtn.addEventListener('click', function(e){
   e.preventDefault()
-  autoraBtn.classList.toggle('arrow-close')
-})
-
-
-const autoraBtn = document.querySelector('.arrow')
-autoraBtn.addEventListener('click', function(e){
-  e.preventDefault()
-  autoraBtn.classList.toggle('arrow-close')
+  autoraBtn.classList.toggle('.arrow-close')
   const autoraInfo = document.querySelector('.secao-autora__about')
-  autoraInfo.classList.toggle('closed');
+  autoraInfo.classList.toggle('opened')
 })
+
+document.addEventListener('mousemove', adaLovelace);
+
+   function adaLovelace(){
+    document.getElementById("efeito").innerHTML = "Era uma vez, uma garota que escreveu a primeira linguagem de programação... 👩‍💻<br>";
+  }
+
